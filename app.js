1981e03d3359ff177d7967bad389cf8e50adeed8
Evgeniya <evgeniya_ignaten@mail.ru>
2024-12-15 19:15:23 +0300

    fix(advanced-1.2): fill in alt-attributes

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='delete-img'
+    deleteButtonImg.alt = 'label and cross inside';
     deleteButton.appendChild(deleteButtonImg);
 
 
